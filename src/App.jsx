0a8653746b31b23a72229d092c2fedd52d7ac827
Nidi-2323 <utp0149290@alumno.utpuebla.edu.mx>
2024-05-06 11:29:49 -0600

    confirmado

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <p>Esta es una modificacion en la rama de Ivan</p>
       </div>
       <p className="read-the-docs">
         Presiona para dirigirte al programa
